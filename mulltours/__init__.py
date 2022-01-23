18107298f2b8c5204d87aafed215c05ec9486ba7
Your Name <you@example.com>
2022-01-22 22:39:53 -0800

    Bump version: 0.1.3 → 0.1.4

--- mulltours/__init__.py
+++ mulltours/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Taylor Monacelli"""
 __email__ = "taylormonacelli@gmail.com"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/TaylorMonacelli/mulltours",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
